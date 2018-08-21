bf0f56ad743f9b56ae2e38441739e412ebbf35e7
michikusa <michikusa.okuribito@gmail.com>
2018-08-21 21:23:18 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello world</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const  App = () => (<Counter></Counter>);
+
+class Counter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {count : 0};
   }
+  
+  handlePlusButton = () =>{
+    this.setState({count : this.state.count + 1});
+  }
+  
+  handleMinusButton = () => {
+    this.setState({count : this.state.count - 1});
+  }
+  render(){
+    return(
+      <React.Fragment> 
+        <div>count :{this.state.count}</div>
+        <button onClick = {this.handlePlusButton}>+1</button>
+        <button onClick = {this.handleMinusButton}>+1</button>
+      </React.Fragment>
+    );
 }
+}
+  
 
 export default App;
