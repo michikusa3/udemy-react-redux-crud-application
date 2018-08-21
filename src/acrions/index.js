4ef554d1f0fb851a427c75207237444b508598a6
michikusa <michikusa.okuribito@gmail.com>
2018-08-21 21:59:21 +0900

    implement action and action creater

---
+++ src/acrions/index.js
@@ -0,0 +1,12 @@
+//アクションクリエイター
+export cosnt INCREMENT = 'INCREMENT';
+export cosnt DECREMENT = 'DECREMENT';
+
+export const increment = () => ({
+        type : INCREMENT
+    })
+
+export const decrement = () => ({
+    type : DECREMENT
+})
+
