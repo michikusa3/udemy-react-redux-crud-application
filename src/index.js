a874d0e294bf1ce402ebcbadad5b69c086eddc47
michikusa <michikusa.okuribito@gmail.com>
2018-08-23 00:31:47 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// reduxから提供されている組み込み関数を入れる
+import { createStore } from 'redux';
+//作成したstoreを全コンポーネントにわたす特殊なコンポーネント
+import { Provider } from 'react-redux';
+
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+// componentは一つのフォルダ内で一括管理したほうがわかりやすい
+import App from './component/App';
 import registerServiceWorker from './registerServiceWorker';
+// アプリ内で唯一　
+// アプリ内の全てのstateはココに集約される
+const store =  createStore(reducer)
+// アプリ内の全てのコンポーネントから参照できるようにする
+// 既存のコンポーネントをProviderコンポーネントでラップする。Store属性に渡す。
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
